8afdb5f52b27b41b8c8c6af0956cf9b655fee51b
Ales <aleswebgit@gmail.com>
2022-03-06 16:57:32 +0100

    useState to set page, view what page is active in menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
-import {MenuItem} from './MenuItem'
+import React, { useState } from 'react'
+import { MenuItem } from './MenuItem'
 
 function Header() {
+  const [page, setPage] = useState('page-1')
+
+  const sections = [
+    { key: 'page-1', title: 'Intro', archive: 'Intro', svg: 'info' },
+    { key: 'page-2', title: 'Sobre Tailwind CSS', archive: 'SobreTailwind', svg: 'about' },
+    { key: 'page-3', title: 'Desventajas y Ventajas', archive: 'ProsContras', svg: 'check' },
+    { key: 'page-4', title: 'Aprender a documentarse', archive: 'Documentarse', svg: 'doc' },
+    { key: 'page-5', title: 'Practica', archive: 'Practica', svg: 'practice' },
+  ]
+
   return (
     <header className="border-r-2 border-blue-300 w-96 fixed h-screen z-20 bg-white flex">
       <div className="flex whitespace-nowrap flex-col w-full">
@@ -11,11 +22,14 @@ function Header() {
 
         <nav className="my-auto pb-32">
           <ul>
-            <MenuItem id="1" text="Intro" />
-            <MenuItem id="2" text="Sobre Tailwind CSS" />
-            <MenuItem id="3" text="Desventajas y Ventajas" />
-            <MenuItem id="4" text="Aprender a documentarse" />
-            <MenuItem id="5" text="Practica" />
+            {sections.map(section => (
+              <MenuItem
+                key={section.key}
+                active={page === section.key}
+                onClick={() => setPage(section.key)}
+                title={section.title}
+              />
+            ))}
           </ul>
         </nav>
       </div>
@@ -25,7 +39,6 @@ function Header() {
           P
         </div>
         <div className="border-y-2 border-blue-300 py-4 px-6 grid justify-center content-center cursor-default">
-          I
         </div>
         <div className="hover:bg-blue-100 cursor-pointer py-4 px-6 grid justify-center hover:text-blue-400 hover:content-center transition">
           N
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,14 @@
-function MenuItem(props) {
+function MenuItem({
+  active,
+  onClick,
+  title,
+}) {
   return (
-    <li className="justify-self-end px-8 py-2 block cursor-pointer hover:text-blue-400 hover:bg-blue-100">
-        {props.text}
+    <li 
+      className={`justify-self-end px-8 py-2 block cursor-pointer hover:text-blue-400 hover:bg-blue-100 ${active ? 'text-blue-400' : ''}`}
+      onClick={onClick}
+    >
+      {title}
     </li>
   )
 }
